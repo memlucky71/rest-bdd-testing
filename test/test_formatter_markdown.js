b588ca33d1b69043f15d3f9c0f1c8830c7cacc85
Seyyed Mohammad Borghei <borghei71@gmail.com>
2019-03-21 19:15:14 +0330

    Add Formatter

---
+++ test/test_formatter_markdown.js
@@ -0,0 +1,57 @@
+/**
+ * Created by mohammad on 3/19/2019 AD.
+ */
+
+const fs = require('fs');
+const chai = require('chai');
+
+import MarkdownFormatter from '../lib/documentary/formatter/markdown';
+
+const expect = chai.expect;
+
+
+describe('Test Markdown Formatter', function() {
+    let docFilePath, markdownFormatter;
+
+    before(function() {
+        docFilePath = `${__dirname}/temp/markdown.md`;
+        markdownFormatter = new MarkdownFormatter(docFilePath);
+    });
+
+    after(function() {
+        fs.unlinkSync(docFilePath);
+    });
+
+    it('Test methods', function(done) {
+        markdownFormatter.writeHeader('This is title level 1');
+        markdownFormatter.writeHeader('This is title level 2', {level: 2});
+        markdownFormatter.writeParagraph('This is paragraph');
+        markdownFormatter.writeList(['Item 1', 'Item 2']);
+        markdownFormatter.writeTable([['a1', 'b1', 'c1'], ['a2', ['b2'], ['c2']]]);
+        markdownFormatter.writeTable([['a1', 'b1', 'c1'], ['a2', ['b2'], ['c2']]], {headers: ['A', 'B', 'C']});
+
+        expect(fs.existsSync(docFilePath)).to.be.true;
+        let docContent = fs.readFileSync(docFilePath, 'utf8');
+        expect(docContent.trim()).to.be.equal(
+`# This is title level 1
+
+## This is title level 2
+
+This is paragraph
+
+* Item 1
+* Item 2
+
+Column1 | Column2 | Column3
+---|---|---
+a1 | b1 | c1
+a2 | b2 | c2
+
+A | B | C
+---|---|---
+a1 | b1 | c1
+a2 | b2 | c2`
+        );
+        done();
+    });
+});
